77baba7d276fc904738d6abaa24037ca1d8d6de9
laza <laza@lazas-macbook-air.local>
2018-09-25 21:48:44 +0200

    Initial Commit

---
+++ testGitXcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testGitXcode
+//
+//  Created by laza on 2018. 09. 25..
+//  Copyright © 2018. laza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
